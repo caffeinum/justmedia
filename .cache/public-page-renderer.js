9eb3d7a6e1b357b5e5efab2e583b59d4132765ff
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-10-09 20:34:58 +0000

    Deploy morejust/justmedia to github.com/morejust/justmedia.git:gh-pages

--- .cache/public-page-renderer.js
+++
@@ -1,9 +0,0 @@
-const preferDefault = m => (m && m.default) || m
-
-if (process.env.NODE_ENV !== `production`) {
-  module.exports = preferDefault(require(`./public-page-renderer-dev`))
-} else if (process.env.BUILD_STAGE === `build-javascript`) {
-  module.exports = preferDefault(require(`./public-page-renderer-prod`))
-} else {
-  module.exports = () => null
-}
